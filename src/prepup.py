29234666747f53c55151e4e9de499037ee8042e9
vishvidooly <vishnu@vidooly.com>
2016-12-28 08:09:06 +0000

    Changed response format

--- src/prepup.py
+++ src/prepup.py
@@ -4,14 +4,17 @@ import urllib
 import json
 import datetime
 from pre_storage import PreLocal
-
+from bottle import request, response
 
 class PgData(object):
     def __init__(self):
         self.video_url = ""
         self.pre_local = PreLocal()
+        self.app_id_pg = 'prepupProgramming'
 
     def get_categories(self):
+         response.content_type = 'application/json;charset=utf-8'
+         response.status = 400
          category_list = []
          try:
               _sql = """
@@ -20,11 +23,16 @@ class PgData(object):
               results = self.pre_local.execute_query(_sql, 'select')
               if results is not None:
                   for cat_id, cat_name in results:
-                      response_data = {}
-                      response_data['cat_id'] = cat_id
-                      response_data['cat_name'] = cat_name
+                      categ_data = {}
+                      categ_data['catId'] = cat_id
+                      categ_data['catName'] = cat_name
                       category_list.append(response_data)
-                  return json.dumps(category_list)
          except Exception as e:
                     return json.dumps({'status': str(e)})
+         response_data = {}
+         response_data['version'] = os.environ.get("PRUP_VER", 'v1')
+         response_data['appId'] = self.app_id_pg
+         response_data['categories'] = json.dumps(category_list)
+         response.status = 200
+         return json.dumps(response_data)
 
